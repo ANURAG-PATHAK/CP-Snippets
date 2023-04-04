f2c1ec4a6bb354aeee4b42f0e40b863ffd993a97
ANURAG-PATHAK <pathakanurag605@gamil.com>
2023-04-04 07:18:37 +0530

    time.cpp added

---
+++ time.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+clock_t tStart = clock();
+int main(){
+    // do stuff
+    printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    return 0;
+}
